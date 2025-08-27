27454a0d1c849b5dc4e944e127202d59c3777fc4
Alexandr Rusin <i@arusin.ru>
2025-08-27 15:39:55 +0700

    change script

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 console.log("[iframe] js run");
 
+const root = document.querySelector("#root");
+
 window.addEventListener("message", function (event) {
-  if (event.origin !== "https://a-rusin.github.io/test-sender/") return;
+  if (event.origin !== "https://a-rusin.github.io/test-sender") return; // без слеша (/)
 
   const data = event.data;
+
+  const newDiv = document.createElement("div");
+  newDiv.textContent = event.data;
+
+  root.appendChild(newDiv);
 
   console.log(data);
 });
