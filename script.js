85b54286c5ad268668b3bdb42939906b0e1f22cf
Alexandr Rusin <i@arusin.ru>
2025-08-27 14:50:32 +0700

    fix script 1

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ console.log("[iframe] js run");
 window.addEventListener(
   "message",
   function (event) {
-    console.log(event);
-    console.log(event.data);
+    if (event.origin === "https://a-rusin.github.io") {
+      console.log(event);
+      console.log(event.data);
+    }
   },
   false
 );
