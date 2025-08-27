7ac9608c9c6792e9305561edd71e4502ec7eaa5e
Alexandr Rusin <i@arusin.ru>
2025-08-27 15:08:52 +0700

    change 2

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 console.log("[iframe] js run");
 
-window.addEventListener(
-  "message",
-  function (event) {
-    if (event.origin === "https://a-rusin.github.io") {
-      console.log(event);
-      console.log(event.data);
-    }
-  },
-  false
-);
+window.addEventListener("message", function (event) {
+  if (event.origin !== "https://a-rusin.github.io/test-sender/") return;
+
+  const data = event.data;
+
+  console.log(data);
+});
